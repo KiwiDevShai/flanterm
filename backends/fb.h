701ff1abc8786d2b914065259d97136320874cce
Mintsuki <mintsuki@protonmail.com>
2025-03-14 14:28:06 +0100

    Add argument identifiers for _free/_malloc in prototypes

--- backends/fb.h
+++ backends/fb.h
@@ -44,8 +44,8 @@ extern "C" {
 
 struct flanterm_context *flanterm_fb_init(
     /* If _malloc and _free are nulled, use the bump allocated instance (1 use only). */
-    void *(*_malloc)(size_t),
-    void (*_free)(void *, size_t),
+    void *(*_malloc)(size_t size),
+    void (*_free)(void *ptr, size_t size),
     uint32_t *framebuffer, size_t width, size_t height, size_t pitch,
     uint8_t red_mask_size, uint8_t red_mask_shift,
     uint8_t green_mask_size, uint8_t green_mask_shift,
